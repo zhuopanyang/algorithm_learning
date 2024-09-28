0d94fa11fce147d21bb4d55fe43775f8e53d4b94
zhuopanyang <15816010612@163.com>
2024-09-28 11:46:20 +0800

    pre-commit

---
+++ leetcode/generate_pop_seq.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*
+
+
+class Solution:
+    """
+    解决出战的所有顺序的类
+    """
+    def __init__(self):
+        self.res = []
+        self.push_seq = ""
+
+    def backtracking(self, stack, current_pop_sequence, next_push_index):
+        """
+        回溯方法
+        :param stack:   一个栈，不断压入字符
+        :param current_pop_sequence:    出栈的字符串序列
+        :param next_push_index: 下一个压入栈的字符
+        :return:    返回所有出栈顺序的可能
+        """
+        # 如果当前出栈序列的长度与入栈序列相同，加入结果
+        if len(current_pop_sequence) == len(self.push_seq):
+            self.res.append("".join(current_pop_sequence))
+            return
+
+        # 如果下一个压入的字符仍然存在，进行入栈操作
+        if next_push_index < len(self.push_seq):
+            # 入栈操作
+            stack.append(self.push_seq[next_push_index])
+            self.backtracking(stack, current_pop_sequence, next_push_index + 1)
+            # 回溯，移除最后压入的元素
+            stack.pop()
+
+        # 如果栈不为空，进行出栈操作
+        if stack:
+            # 出栈操作
+            current_pop_sequence.append(stack.pop())
+            self.backtracking(stack, current_pop_sequence, next_push_index)
+            # 回溯，移除最后出栈的元素
+            stack.append(current_pop_sequence.pop())
+
+    def main(self, push_sequence):
+        """
+        主方法，处理数据，调用回溯方法，进行返回
+        :param push_sequence:
+        :return:
+        """
+        self.res = []
+        self.push_seq = push_sequence
+
+        # 初始化栈和当前出栈序列
+        self.backtracking([], [], 0)
+        return self.res
+
+
+if __name__ == '__main__':
+    # 定义入栈的顺序
+    push_sequence = "ABCD"
+    test = Solution()
+
+    # 生成所有可能的出栈顺序
+    pop_sequences = test.main(push_sequence)
+
+    # 输出结果
+    print("所有可能的出栈顺序:")
+    for sequence in pop_sequences:
+        print(sequence)
---
+++ leetcode/sqrt_num.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*
+
+
+def sqrt_binary_search(target, precision):
+    """
+    计算一个数字的根号，保留两位小数点
+    :param target:  需要计算的数字
+    :param precision:   需要保留的小数点
+    :return:    返回保留小数点后的数字的根号
+    """
+    left = 0
+    right = target
+    mid = 0
+
+    while right - left > precision:  # 保持精确度，用 []
+        mid = (left + right) / 2
+        mid_squared = mid * mid
+
+        if mid_squared < target:  # mid 的平方小于目标
+            left = mid
+        else:  # mid 的平方大于等于目标
+            right = mid
+
+    return round(mid, 2)  # 保留两位小数
+
+
+if __name__ == '__main__':
+    # 要求解根号的数字
+    number = 10
+    # 精确度到小数点后两位
+    precision = 0.01
+
+    result = sqrt_binary_search(number, precision)
+    print(f"根号{number}为：{result}")
